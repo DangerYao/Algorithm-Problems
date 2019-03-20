8322f289808801eb8765b5ff8a48d4af36162810
Danger Yao <1187069887@qq.com>
2019-03-20 12:18:22 +0800

    fix SteelCutting

--- src/main/java/com/mixky/danger/algorithm/dp/SteelCutting.java
+++ src/main/java/com/mixky/danger/algorithm/dp/SteelCutting.java
@@ -13,8 +13,9 @@ public class SteelCutting {
 
 	public static void main(String[] args) {
 		int[] p = new int[] {1,5,8,9,10,17,17,20,24,30};
-		int n = 9;
+		int n = 6;
 		System.out.println(cutWithRecursion(p, n));
+		System.out.println(catWithDp(p, n));
 	}
 
 	/**
@@ -27,6 +28,9 @@ public class SteelCutting {
 		if(n == 0) {
 			return 0;
 		}
+		if(n > p.length) {
+			throw new RuntimeException("n暂不支持大于数组的长度");
+		}
 		
 		int q = Integer.MIN_VALUE;
 		for (int i=1; i<=n; i++) {
@@ -34,6 +38,36 @@ public class SteelCutting {
 		}
 		
 		return q;
+	}
+	
+	/**
+	 * 	采用动态规划求解
+	 * @param p 价格表数组
+	 * @param n 需要分割的长度
+	 * @return
+	 */
+	public static int catWithDp(int[] p, int n) {
+		
+		if(n == 0) {
+			return 0;
+		}
+		if(n > p.length) {
+			throw new RuntimeException("n暂不支持大于数组的长度");
+		}
+		
+		int[] dp = new int[p.length+1];
+		
+		for(int i=1;i<=n; i++) {
+			int q = Integer.MIN_VALUE;
+			
+			// 获取子问题的最优解 
+			for(int j=1;j<=i;j++) {
+				q = Math.max(q, dp[i-j] + p[j-1]);
+			}
+			dp[i] = q;
+		}
+		
+		return dp[n];
 	}
 	
 	
